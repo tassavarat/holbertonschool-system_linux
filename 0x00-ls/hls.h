2a06c151f8e9a2c105b73e59280d97c8f97d83c1
Tim Assavarat <tassavarat@gmail.com>
2020-01-29 13:25:01 -0800

    Updating header for multiple source files

--- 0x00-ls/hls.h
+++ 0x00-ls/hls.h
@@ -5,10 +5,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <errno.h>
 
 struct content
 {
 	char name[256];
 };
+
+/* string */
+int _strcmp(char *s1, char *s2);
+char *str_toupper(char *s);
+char *_strcpy(char *dest, const char *src);
+int cmpstringp(const char *p1, const char *p2);
+
+/* sort */
+void swap(struct content **entries, int i, int j);
+int partition(struct content **entries, int lo, int hi);
+void _qsort(struct content **entries, int lo, int hi);
 
 #endif /* HLS_H */
