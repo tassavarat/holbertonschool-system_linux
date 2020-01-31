18f8a8ec21a35055ab2a6360bdfbfe3640de8df5
tassavarat <tassavarat@gmail.com>
2020-01-30 23:30:43 -0800

    Updating header for additional functions and added documentation

--- 0x00-ls/hls.h
+++ 0x00-ls/hls.h
@@ -2,28 +2,46 @@
 #define HLS_H
 
 #include <dirent.h>
+#include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <errno.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
+/**
+ * struct content - stores entry information
+ * @name: string buffer
+ */
 typedef struct content
 {
 	char name[256];
 } content_t;
 
+/* main */
+void error(char *argv, char o);
+
+/* ls */
+void ls(const int argc, char *argv[]);
+content_t *preprocess(const int argc, unsigned int *numdir, char *argv[],
+		char *option_a);
+unsigned int parse_args(unsigned int *numdir, char *argv[], char *option_a,
+		int *file_a, int *dir_a);
+content_t *handlecontent(const bool f, const unsigned int c, char *argv[],
+		int *a);
+void printcontent(const int argc, char *argv, const unsigned int c,
+		struct content *entries);
+
 /* string */
-int _strcmp(char *s1, char *s2);
-char *str_toupper(char *s);
 char *_strcpy(char *dest, const char *src);
 int cmpstringp(const char *p1, const char *p2);
+char *str_toupper(char *s);
+int _strcmp(char *s1, char *s2);
 
 /* sort */
-void swap(struct content **entries, int i, int j);
-int partition(struct content **entries, int lo, int hi);
 void _qsort(struct content **entries, int lo, int hi);
+int partition(struct content **entries, int lo, int hi);
+void swap(struct content **entries, int i, int j);
 
 #endif /* HLS_H */
