876289ceeb2e9b9207e01a723acf73768b2c9836
Tim Assavarat <tassavarat@gmail.com>
2020-01-31 23:19:55 -0800

    Moving handlecontent to main

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -12,17 +12,18 @@ int main(int argc, char *argv[])
 	unsigned int status;
 
 	status = ls(argc, argv);
-	exit(status);
+	return (status);
 }
 
 /**
  * error - prints error messages
+ * @e: whether function should exit
  * @argv: string containing invalid argument
  * @o: character containing invalid ls option
  *
  * Return: error status
  */
-unsigned int error(char *argv, char o)
+unsigned int error(bool e, char *argv, char o)
 {
 	char buf[BUFSIZ];
 
@@ -31,12 +32,40 @@ unsigned int error(char *argv, char o)
 	{
 		fprintf(stderr, "ls: invalid option -- '%c'\n", o);
 		fprintf(stderr, "Try 'ls --help' for more information.\n");
-		exit(2);
 	}
-	if (errno == ENOENT)
+	else if (errno == ENOENT)
 		sprintf(buf, "ls: cannot access %s", argv);
 	else if (errno == EACCES)
 		sprintf(buf, "ls: cannot open directory %s", argv);
 	perror(buf);
+	if (e)
+		exit(2);
 	return (2);
+}
+
+/**
+ * handlecontent - allocates space for struct containing content information
+ * @f: if content is a file
+ * @c: count of content
+ * @argv: pointer to array of strings containing name of content
+ * @a: pointer to array of integers containing index of content
+ *
+ * Return: created struct
+ */
+content_t *handlecontent(const bool f, const unsigned int c, char *argv[],
+		int *a)
+{
+	unsigned int i;
+	struct content *entries;
+
+	entries = malloc(c * sizeof(*entries));
+	for (i = 0; i < c; ++i)
+		_strcpy(entries[i].name, argv[a[i]]);
+	if (f)
+	{
+		printcontent(0, NULL, c, entries);
+		free(entries);
+		return (NULL);
+	}
+	return (entries);
 }
