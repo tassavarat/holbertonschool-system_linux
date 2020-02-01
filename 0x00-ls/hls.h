707ae2633ffa7c27bae33f6f245b0bd4f7afd9af
tassavarat <tassavarat@gmail.com>
2020-02-01 15:51:42 -0800

    Updating documentation and added new prototypes

--- 0x00-ls/hls.h
+++ 0x00-ls/hls.h
@@ -19,15 +19,41 @@ typedef struct content
 	char name[256];
 } content_t;
 
+/**
+ * struct option - stores options state
+ * @perline: Whether to print contents per line
+ * @hidden: Whether to show hidden contents
+ * @hiddenavigation: Whether to omit . and ..
+ * @longfmt: Whether to show lstat information
+ * @rev: Whether to sort in reverse
+ * @sortsize: Whether to sort by content size
+ * @sorttime: Whether to sort by modification time
+ * @recurs: Whether list subdirectories recursively
+ */
+typedef struct option
+{
+	bool perline;
+	bool hidden;
+	bool hiddenavigation;
+	bool longfmt;
+	bool rev;
+	bool sortsize;
+	bool sorttime;
+	bool recurs;
+} option_t;
+
 /* main */
 unsigned int error(bool e, char *argv, char o);
+void initoptions(struct option **opt);
+void checkoptions(struct option **opt, char *argv[], const unsigned int i,
+		const unsigned int j);
 
 /* ls */
 unsigned int ls(const int argc, char *argv[]);
 content_t *preprocess(const int argc, char *argv[], unsigned int *numdir,
-		DIR **dp, char *option_a);
-unsigned int parse_args(unsigned int *numdir, char *argv[], char *option_a,
-		int *file_a, int *dir_a);
+		DIR **dp);
+unsigned int parse_args(unsigned int *numdir, char *argv[], int *file_a,
+		int *dir_a);
 content_t *handlecontent(const bool f, const unsigned int c, char *argv[],
 		int *a);
 void printcontent(const bool f, const int argc, char *argv,
