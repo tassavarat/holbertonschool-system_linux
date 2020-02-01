c429dfcdb2cc042127ebf5a6efdc6d677421b0ab
tassavarat <tassavarat@gmail.com>
2020-02-01 15:52:15 -0800

    Implementing initoptions and checkoptions functions

--- 0x00-ls/ls.c
+++ 0x00-ls/ls.c
@@ -2,6 +2,7 @@
 
 static bool start;
 static unsigned int status;
+static struct option *opt;
 
 /**
  * ls - Lists information about files and directories
@@ -14,12 +15,11 @@ unsigned int ls(const int argc, char *argv[])
 {
 	unsigned int entry_size = 100, dc, ec, i;
 	DIR *dp;
-	char option_a[256];
 	struct dirent *ep;
 	struct content *entries, *dirs;
 
 	dc = ec = 0;
-	dirs = preprocess(argc, argv, &dc, &dp, option_a);
+	dirs = preprocess(argc, argv, &dc, &dp);
 	entries = malloc(entry_size * sizeof(*entries));
 	for (i = 0; i < dc || argc == 1; ++i, ec = 0)
 	{
@@ -60,17 +60,19 @@ unsigned int ls(const int argc, char *argv[])
  * @argv: pointer to an array of strings to process
  * @dp: pointer to pointer of struct DP containing lstat information
  * @numdir: number of directories
- * @option_a: pointer to a character array to contain index of options
  *
  * Return: struct containing directory information
  */
 content_t *preprocess(const int argc, char *argv[], unsigned int *numdir,
-		DIR **dp, char *option_a)
+		DIR **dp)
 {
 	unsigned int numfiles;
 	int file_a[256], dir_a[256];
 	struct content *dirs;
 
+	opt = malloc(sizeof(*opt));
+	/* free me */
+	initoptions(&opt);
 	if (argc == 1)
 	{
 		*dp = opendir(".");
@@ -79,7 +81,7 @@ content_t *preprocess(const int argc, char *argv[], unsigned int *numdir,
 	}
 	else
 	{
-		numfiles = parse_args(numdir, argv, option_a, file_a, dir_a);
+		numfiles = parse_args(numdir, argv, file_a, dir_a);
 		if (numfiles > 0)
 		{
 			handlecontent(true, numfiles, argv, file_a);
@@ -98,50 +100,28 @@ content_t *preprocess(const int argc, char *argv[], unsigned int *numdir,
  * parse_args - indexes position of valid arguments, files, directories
  * @numdir: number of directories
  * @argv: pointer to an array of strings to parse
- * @option_a: pointer to a character array to populate with index of options
  * @file_a: pointer to an array of characters to populate with index of files
  * @dir_a: pointer an array of characters to populate with index of directories
  *
  * Return: number of files
  */
-unsigned int parse_args(unsigned int *numdir, char *argv[], char *option_a,
-		int *file_a, int *dir_a)
+unsigned int parse_args(unsigned int *numdir, char *argv[], int *file_a,
+		int *dir_a)
 {
-	char vo[8] = {'1', 'a', 'A', 'l', 'r', 'S', 't', 'R'};
-	unsigned int i, j, k, l, numfiles, argsize;
+	unsigned int i, j, numfiles;
 	struct stat sb;
 
-	k = numfiles = 0;
-	argsize = 8;
 	for (i = 1; argv[i]; ++i)
 	{
 		if (*argv[i] == '-')
-		{
 			for (j = 1; argv[i][j]; ++j)
-			{
-				for (l = 0; l < argsize; ++l)
-					if (argv[i][j] == vo[l])
-					{
-						option_a[k] = argv[i][j];
-						break;
-					}
-				if (l == argsize)
-					error(true, NULL, argv[i][j]);
-				++k;
-			}
-		}
+				checkoptions(&opt, argv, i, j);
 		else if (lstat(argv[i], &sb) == -1)
-		{
 			status = error(false, argv[i], '\0');
-		}
 		else if ((sb.st_mode & S_IFMT) == S_IFREG)
-		{
 			file_a[numfiles++] = i;
-		}
 		else if ((sb.st_mode & S_IFMT) == S_IFDIR)
-		{
 			dir_a[(*numdir)++] = i;
-		}
 	}
 	return (numfiles);
 }
@@ -196,7 +176,7 @@ void printcontent(const bool f, const int argc, char *argv,
 	for (i = 0; i < c; ++i)
 	{
 		if (start)
-			printf("  ");
+			!opt->perline ? printf(" ") : printf("\n");
 		printf("%s", entries[i].name);
 		start = true;
 	}
--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -43,3 +43,60 @@ unsigned int error(bool e, char *argv, char o)
 		exit(2);
 	return (2);
 }
+
+/**
+ * initoptions - initialises option struct
+ * @opt: struct to initialise
+ */
+void initoptions(struct option **opt)
+{
+	(*opt)->perline = false;
+	(*opt)->hidden = false;
+	(*opt)->hiddenavigation = false;
+	(*opt)->longfmt = false;
+	(*opt)->rev = false;
+	(*opt)->sortsize = false;
+	(*opt)->sorttime = false;
+	(*opt)->recurs = false;
+}
+
+/**
+ * checkoptions - checks for valid options and turns them on as necessary
+ * @opt: structure of bools
+ * @argv: string to check for options
+ * @i: first index
+ * @j: second index
+ */
+void checkoptions(struct option **opt, char *argv[], const unsigned int i,
+		const unsigned int j)
+{
+	switch (argv[i][j])
+	{
+		case '1':
+			(*opt)->perline = true;
+			break;
+		case 'a':
+			(*opt)->hidden = true;
+			break;
+		case 'A':
+			(*opt)->hiddenavigation = true;
+			break;
+		case 'l':
+			(*opt)->longfmt = true;
+			break;
+		case 'r':
+			(*opt)->rev = true;
+			break;
+		case 'S':
+			(*opt)->sortsize = true;
+			break;
+		case 't':
+			(*opt)->sorttime = true;
+			break;
+		case 'R':
+			(*opt)->recurs = true;
+			break;
+		default:
+			error(true, NULL, argv[i][j]);
+	}
+}
