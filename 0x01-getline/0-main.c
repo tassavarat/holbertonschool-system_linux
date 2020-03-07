eb8e1e725210ce90610ef75c86eb9c05947bd688
Tim Assavarat <tassavarat@gmail.com>
2020-03-06 18:41:16 -0800

    Adding main files

---
+++ 0x01-getline/0-main.c
@@ -0,0 +1,37 @@
+#include "laps.h"
+
+/**
+ * main - entry point.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+
+	/* int ids1[3] = {1, 42, 101}; */
+	int ids1[4] = {2, 42, 101, 1};
+	int ids2[1] = {11};
+
+	race_state(ids1, 4);
+	printf("--\n");
+	race_state(ids1, 4);
+	printf("--\n");
+	race_state(ids1, 4);
+	printf("--\n");
+	race_state(ids2, 1);
+	printf("--\n");
+	race_state(ids1, 4);
+	printf("--\n");
+	race_state(ids2, 1);
+	printf("--\n");
+	race_state(ids1, 4);
+	printf("--\n");
+	race_state(ids2, 1);
+	printf("--\n");
+	race_state(ids1, 4);
+	printf("--\n");
+	race_state(ids2, 1);
+	printf("--\n");
+	race_state(NULL, 0);
+	return (0);
+}
