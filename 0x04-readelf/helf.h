a5350c915133ec2982311866ee48313f9cea8a5d
tassavarat <tassavarat@gmail.com>
2020-03-18 19:14:28 -0700

    Adding prototypes for printing of header entries

--- 0x04-readelf/helf.h
+++ 0x04-readelf/helf.h
@@ -7,6 +7,7 @@
 
 /* printelfh */
 int printelfh(FILE *fp, char *arg_str);
+int printversion(Elf64_Ehdr hdr, char *arg_str);
 void printosabi(Elf64_Ehdr hdr);
 void printtype(Elf64_Ehdr hdr);
 void printmachine(Elf64_Ehdr hdr);
@@ -16,5 +17,8 @@ void printpshoff(Elf64_Ehdr hdr);
 void printflags(Elf64_Ehdr hdr);
 void printhdrsize(Elf64_Ehdr hdr);
 void printphnum(Elf64_Ehdr hdr);
+void printshentsize(Elf64_Ehdr hdr);
+void printshnum(Elf64_Ehdr hdr);
+void printshstrndx(Elf64_Ehdr hdr);
 
 #endif /* HELF_H */
