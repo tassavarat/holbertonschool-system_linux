eac89a6203b25006e1a33f84c87be7173a6e890d
tassavarat <tassavarat@gmail.com>
2020-03-19 16:29:31 -0700

    Adding struct for 32-bit compatability

--- 0x04-readelf/helf.h
+++ 0x04-readelf/helf.h
@@ -4,21 +4,28 @@
 #include <elf.h>
 #include <stdio.h>
 #include <string.h>
+/* #include <sys/stat.h> */
+
+typedef struct
+{
+	Elf32_Ehdr hdr32;
+	Elf64_Ehdr hdr64;
+} hdrs;
 
 /* printelfh */
-int printelfh(FILE *fp, char *arg_str);
-int printversion(Elf64_Ehdr hdr, char *arg_str);
-void printosabi(Elf64_Ehdr hdr);
-void printtype(Elf64_Ehdr hdr);
-void printmachine(Elf64_Ehdr hdr);
-void printfileversion(Elf64_Ehdr hdr);
-void printentry(Elf64_Ehdr hdr);
-void printpshoff(Elf64_Ehdr hdr);
-void printflags(Elf64_Ehdr hdr);
-void printhdrsize(Elf64_Ehdr hdr);
-void printphnum(Elf64_Ehdr hdr);
-void printshentsize(Elf64_Ehdr hdr);
-void printshnum(Elf64_Ehdr hdr);
-void printshstrndx(Elf64_Ehdr hdr);
+int printelfh(FILE *fp, FILE *fp2, char *arg_str);
+int printversion(hdrs hdr, char *arg_str);
+void printosabi(hdrs hdr);
+void printtype(hdrs hdr);
+void printmachine(hdrs hdr);
+void printfileversion(hdrs hdr);
+void printentry(hdrs hdr);
+void printpshoff(hdrs hdr);
+void printflags(hdrs hdr);
+void printhdrsize(hdrs hdr);
+void printphnum(hdrs hdr);
+void printshentsize(hdrs hdr);
+void printshnum(hdrs hdr);
+void printshstrndx(hdrs hdr);
 
 #endif /* HELF_H */
