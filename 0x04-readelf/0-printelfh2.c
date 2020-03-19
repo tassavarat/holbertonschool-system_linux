abf85b4c2c2ec38f8f0e2c76d16db67c500eec9f
tassavarat <tassavarat@gmail.com>
2020-03-18 18:17:24 -0700

    Made print formatting more dynamic

--- 0x04-readelf/0-printelfh2.c
+++ 0x04-readelf/0-printelfh2.c
@@ -6,7 +6,7 @@
  */
 void printfileversion(Elf64_Ehdr hdr)
 {
-	printf("%10s%29s%x\n", "Version:", "0x",
+	printf("%9s%-28c%#x\n", "Version", ':',
 			hdr.e_version == EV_NONE ? EV_NONE : EV_CURRENT);
 }
 
@@ -19,28 +19,28 @@ void printmachine2(Elf64_Half machine)
 	switch (machine)
 	{
 		case EM_PPC64:
-			printf("%41s\n", "PowerPC 64-bit");
+			printf("%s\n", "PowerPC 64-bit");
 			break;
 		case EM_S390:
-			printf("%35s\n", "IBM S390");
+			printf("%s\n", "IBM S390");
 			break;
 		case EM_ARM:
-			printf("%30s\n", "ARM");
+			printf("%s\n", "ARM");
 			break;
 		case EM_SH:
-			printf("%37s\n", "Hitachi SH");
+			printf("%s\n", "Hitachi SH");
 			break;
 		case EM_SPARCV9:
-			printf("%42s\n", "SPARC v9 64-bit");
+			printf("%s\n", "SPARC v9 64-bit");
 			break;
 		case EM_IA_64:
-			printf("%39s\n", "Intel Merced");
+			printf("%s\n", "Intel Merced");
 			break;
 		case EM_X86_64:
-			printf("%50s\n", "AMD x86-64 architecture");
+			printf("%s\n", "Advanced Micro Devices X86-64");
 			break;
 		case EM_VAX:
-			printf("%38s\n", "Digital VAX");
+			printf("%s\n", "Digital VAX");
 	}
 }
 
@@ -50,41 +50,41 @@ void printmachine2(Elf64_Half machine)
  */
 void printmachine(Elf64_Ehdr hdr)
 {
-	printf("%10s", "Machine:");
+	printf("%9s%-28c", "Machine", ':');
 	switch (hdr.e_machine)
 	{
 		case EM_NONE:
-			printf("%34s\n", "EM_NONE");
+			printf("%s\n", "EM_NONE");
 			break;
 		case EM_M32:
-			printf("%40s\n", "AT&T WE 32100");
+			printf("%s\n", "AT&T WE 32100");
 			break;
 		case EM_SPARC:
-			printf("%36s\n", "SUN SPARC");
+			printf("%s\n", "SUN SPARC");
 			break;
 		case EM_386:
-			printf("%33s\n", "EM_386");
+			printf("%s\n", "EM_386");
 			break;
 		case EM_68K:
-			printf("%33s\n", "EM_68K");
+			printf("%s\n", "EM_68K");
 			break;
 		case EM_88K:
-			printf("%47s\n", "Motorola m88k family");
+			printf("%s\n", "Motorola m88k family");
 			break;
 		case EM_860:
-			printf("%38s\n", "Intel 80860");
+			printf("%s\n", "Intel 80860");
 			break;
 		case EM_MIPS:
-			printf("%48s\n", "MIPS R3000 big-endian");
+			printf("%s\n", "MIPS R3000 big-endian");
 			break;
 		case EM_PARISC:
-			printf("%31s\n", "HPPA");
+			printf("%s\n", "HPPA");
 			break;
 		case EM_SPARC32PLUS:
-			printf("%41s\n", "Sun's \"v8plus\"");
+			printf("%s\n", "Sun's \"v8plus\"");
 			break;
 		case EM_PPC:
-			printf("%34s\n", "PowerPC");
+			printf("%s\n", "PowerPC");
 			break;
 		default:
 			printmachine2(hdr.e_machine);
@@ -97,23 +97,23 @@ void printmachine(Elf64_Ehdr hdr)
  */
 void printtype(Elf64_Ehdr hdr)
 {
-	printf("%7s", "Type:");
+	printf("%6s%-31c", "Type", ':');
 	switch (hdr.e_type)
 	{
 		case ET_NONE:
-			printf("%49s\n", "NONE (No file type)");
+			printf("%s\n", "NONE (No file type)");
 			break;
 		case ET_REL:
-			printf("%52s\n", "REL (Relocatable file)");
+			printf("%s\n", "REL (Relocatable file)");
 			break;
 		case ET_EXEC:
-			printf("%44s\n", "EXEC (ET_EXEC)");
+			printf("%s\n", "EXEC (ET_EXEC)");
 			break;
 		case ET_DYN:
-			printf("%54s\n", "DYN (Shared object file)");
+			printf("%s\n", "DYN (Shared object file)");
 			break;
 		case ET_CORE:
-			printf("%46s\n", "CORE (Core file)");
+			printf("%s\n", "CORE (Core file)");
 	}
 }
 
@@ -123,7 +123,7 @@ void printtype(Elf64_Ehdr hdr)
  */
 void printosabi(Elf64_Ehdr hdr)
 {
-	printf("%10s%34s", "OS/ABI: ", "UNIX - ");
+	printf("%8s%-29c%s", "OS/ABI", ':', "UNIX - ");
 	switch (hdr.e_ident[EI_OSABI])
 	{
 		case ELFOSABI_NONE:
