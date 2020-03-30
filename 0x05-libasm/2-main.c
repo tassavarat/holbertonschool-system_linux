32f85818fc5a06585d4161fd3160b7a0b5df8b17
tassavarat <tassavarat@gmail.com>
2020-03-29 20:33:20 -0700

    Updating main

--- 0x05-libasm/2-main.c
+++ 0x05-libasm/2-main.c
@@ -16,12 +16,29 @@
  */
 int main(void)
 {
+	/* char *str1 = "Betty"; */
+	/* char *str2 = "Batty"; */
+
 	/* printf("%i\n", strncmp(S1, S3, 1)); */
 	/* printf("%i\n", asm_strncmp(S1, S3, 1)); */
 	/* printf("%i\n", strncmp(S1, S3, 11)); */
 	/* printf("%i\n", asm_strncmp(S1, S3, 11)); */
 	/* printf("%i\n", strncmp(S1, S3, 15)); */
 	/* printf("%i\n", asm_strncmp(S1, S3, 15)); */
+	/* printf("%i\n", strncmp(S1, S1, 16)); */
+	/* printf("%i\n", asm_strncmp(S1, S1, 16)); */
+	/* printf("%i\n", strncmp(S1, S2, 1)); */
+	/* printf("%i\n", asm_strncmp(S1, S2, 1)); */
+	/* printf("%i\n", strncmp(S1, S2, 0)); */
+	/* printf("%i\n", asm_strncmp(S1, S2, 0)); */
+	/* printf("%i\n", strncmp("Betty", "Betto", 5)); */
+	/* printf("%i\n", asm_strncmp("Betty", "Betto", 5)); */
+	/* printf("%i\n", strncmp("Yetty", "Betto", 5)); */
+	/* printf("%i\n", asm_strncmp("Yetty", "Betto", 5)); */
+	/* printf("%i\n", strncmp(str1, str2, 5)); */
+	/* printf("%i\n", asm_strncmp(str1, str2, 5)); */
+	/* printf("%i\n", strncmp(str1, str1, 5)); */
+	/* printf("%i\n", asm_strncmp(str1, str1, 5)); */
 	assert(strncmp(S1, S3, 1) == asm_strncmp(S1, S3, 1));
 	assert(strncmp(S1, S3, 11) == asm_strncmp(S1, S3, 11));
 	assert(strncmp(S1, S3, 15) == asm_strncmp(S1, S3, 15));
