4b435e0be1ef9f7de6159d834e0b53642b735434
tassavarat <tassavarat@gmail.com>
2020-03-29 20:29:31 -0700

    Reverting ret behavior to -1, 0, 1

--- 0x05-libasm/2-strncmp.asm
+++ 0x05-libasm/2-strncmp.asm
@@ -5,33 +5,38 @@ section .text
 asm_strncmp:
 	push rbp
 	mov rbp, rsp
-	push r8
-	push r9
 	push rcx
 
 	xor rcx, rcx
-	xor r8, r8
-	xor r9, r9
-	xor eax, eax
 loop_asm_strncmp:
 	cmp rcx, rdx
-	je diff
-	mov r8b, [rdi + rcx]
-	mov r9b, [rsi + rcx]
-	test r8b, r8b
-	jz diff
-	test r9b, r9b
-	jz diff
-	cmp r8b, r9b
-	jne diff
+	je compare
+	mov al, [rdi + rcx]
+	mov bl, [rsi + rcx]
+	test al, al
+	jz compare
+	test bl, bl
+	jz compare
+	cmp al, bl
+	jne compare
 	inc rcx
 	jmp loop_asm_strncmp
-diff:
-	mov eax, r8d
-	sub eax, r9d
+compare:
+	cmp al, bl
+	je equal
+	jl less
+	jg greater
+equal:
+	mov rax, 0
+	jmp end
+less:
+	mov rax, -1
+	jmp end
+greater:
+	mov rax, 1
+	jmp end
+end:
 	pop rcx
-	pop r9
-	pop r8
 	mov rsp, rbp
 	pop rbp
 	ret
