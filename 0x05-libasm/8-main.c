d9cf79e7666028ce3659eaa0633b660fa795d4aa
tassavarat <tassavarat@gmail.com>
2020-04-01 21:15:39 -0700

    Implementing asm_strcasecmp

---
+++ 0x05-libasm/8-main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "libasm.h"
+
+#define S1  "Holberton School"
+#define S2  "HOLBERTON SCHOOL"
+#define S3  "Holberton SchooL"
+#define S4  "holberton socool"
+
+/**
+ * main - Program entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+	/* printf("%i\n", strcasecmp(S1, S1)); */
+	/* printf("%i\n", asm_strcasecmp(S1, S1)); */
+	/* printf("%i\n", strcasecmp(S1, S2)); */
+	/* printf("%i\n", asm_strcasecmp(S1, S2)); */
+	/* printf("%i\n", strcasecmp(S1, S3)); */
+	/* printf("%i\n", asm_strcasecmp(S1, S3)); */
+	/* printf("%i\n", strcasecmp(S1, S4)); */
+	/* printf("%i\n", asm_strcasecmp(S1, S4)); */
+	assert(strcasecmp(S1, S1) == asm_strcasecmp(S1, S1));
+	assert(strcasecmp(S1, S2) == asm_strcasecmp(S1, S2));
+	assert(strcasecmp(S1, S3) == asm_strcasecmp(S1, S3));
+	assert(strcasecmp(S1, S4) == asm_strcasecmp(S1, S4));
+
+	printf("All good!\n");
+	return (EXIT_SUCCESS);
+}
---
+++ 0x05-libasm/8-strcasecmp.asm
@@ -0,0 +1,56 @@
+BITS 64
+
+global asm_strcasecmp
+
+asm_strcasecmp:
+	push rbp
+	mov rbp, rsp
+	push rcx
+	push r8
+	push r9
+
+	xor rcx, rcx
+	xor r8, r8
+	xor r9, r9
+	xor eax, eax
+loop_asm_strcasecmp:
+	mov r8b, [rdi + rcx]
+	mov r9b, [rsi + rcx]
+	test r8b, r8b
+	jz dif
+	test r9b, r9b
+	jz dif
+	cmp r8b, 65		; rdi + rcx >= 'A'
+	jge check_8_end
+check_9:
+	cmp r9b, 65		; rsi + rcx >= 'A'
+	jge check_9_end
+cont:
+	cmp r8b, r9b
+	jne dif
+	inc rcx
+	jmp loop_asm_strcasecmp
+check_8_end:
+	cmp r8b, 90		; rdi + rcx <= 'Z'
+	jle conv_8
+	jmp check_9
+check_9_end:
+	cmp r9b, 90		; rsi + rcx <= 'Z'
+	jle conv_9
+	jmp cont
+conv_8:
+	add r8b, 32
+	jmp check_9
+conv_9:
+	add r9b, 32
+	jmp cont
+dif:
+	mov eax, r8d
+	sub eax, r9d
+end:
+	pop r9
+	pop r8
+	pop rcx
+	mov rsp, rbp
+	pop rbp
+	ret
