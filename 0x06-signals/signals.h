39aeaca0372e59aacc5aed47182587522f92d360
tassavarat <tassavarat@gmail.com>
2020-04-08 16:36:25 -0700

    Header for project

--- 0x06-signals/signals.h
+++ 0x06-signals/signals.h
@@ -1,6 +1,10 @@
 #ifndef SIGNALS_H
 #define SIGNALS_H
 
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+
 int handle_signal(void);
 void (*current_handler_signal(void))(int);
 int handle_sigaction(void);
