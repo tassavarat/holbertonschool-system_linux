3b10c86e4cc803b5416808c7aa68a5c4c81c6576
tassavarat <tassavarat@gmail.com>
2020-04-08 13:51:10 -0700

    Header for project

---
+++ 0x06-signals/signals.h
@@ -0,0 +1,16 @@
+#ifndef SIGNALS_H
+#define SIGNALS_H
+
+int handle_signal(void);
+void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
+int pid_exist(pid_t pid);
+void all_in_one(void);
+int sigset_init(sigset_t *set, int *signals);
+int signals_block(int *signals);
+int signals_unblock(int *signals);
+int handle_pending(void (*handler)(int));
+
+#endif /* SIGNALS_H */
