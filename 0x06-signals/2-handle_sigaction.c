c55385a00f2d6de1a6cdd88556c643713b9f308b
tassavarat <tassavarat@gmail.com>
2020-04-09 19:02:14 -0700

    Updating documentation

--- 0x06-signals/2-handle_sigaction.c
+++ 0x06-signals/2-handle_sigaction.c
@@ -1,10 +1,19 @@
 #include "signals.h"
 
+/**
+ * siginthandler - action to take on SIGINT delivery
+ * @signum: signal value to print
+ */
 void siginthandler(int signum)
 {
 	printf("Gotcha! [%i]\n", signum);
 }
 
+/**
+ * handle_sigaction - Sets a handler for SIGINT
+ *
+ * Return: 1 on success, -1 on error
+ */
 int handle_sigaction(void)
 {
 	struct sigaction act;
