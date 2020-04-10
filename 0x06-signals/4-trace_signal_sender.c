5f0cc62fa42a7027573167b6134881984e0647c3
tassavarat <tassavarat@gmail.com>
2020-04-09 19:35:54 -0700

    Implementing trace_signal_sender

---
+++ 0x06-signals/4-trace_signal_sender.c
@@ -0,0 +1,28 @@
+#include "signals.h"
+
+/**
+ * sigquithandler - action to take on SIGQUIT intercept
+ * @sig: number of the signal that caused invocation of the handler
+ * @info: pointer to a siginfo_t
+ * @ucontext: pointer to a ucontext_t
+ */
+void sigquithandler(int sig, siginfo_t *info, void *ucontext)
+{
+	(void) sig;
+	(void) ucontext;
+	printf("SIGQUIT sent by %i\n", info->si_pid);
+}
+
+/**
+ * trace_signal_sender - Defines a handler for SIGQUIT
+ *
+ * Return: 0 on success, -1 on error
+ */
+int trace_signal_sender(void)
+{
+	struct sigaction act;
+
+	act.sa_flags = SA_SIGINFO;
+	act.sa_sigaction = sigquithandler;
+	return (sigaction(SIGQUIT, &act, NULL));
+}
