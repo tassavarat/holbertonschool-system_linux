6a20f5ec3eacb04c154abf98a42223bf50cf97e6
tassavarat <tassavarat@gmail.com>
2020-04-11 16:12:40 -0700

    Implementing signal send

---
+++ 0x06-signals/7-signal_send.c
@@ -0,0 +1,19 @@
+#include "signals.h"
+
+/**
+ * main - Sends SIGINT to a process using its PID
+ * @argc: count of arguments
+ * @argv: arguments passed
+ *
+ * Return: EXIT_SUCCESS on success, otherwise EXIT_FAILURE
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s <pid>\n", *argv);
+		return (EXIT_FAILURE);
+	}
+	kill(atoi(argv[1]), SIGINT);
+	return (EXIT_SUCCESS);
+}
