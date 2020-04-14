50c7c1ee2e05b55cb381dec2ee6b74bf1968f4e1
tassavarat <tassavarat@gmail.com>
2020-04-13 21:51:07 -0700

    Implementing signal block

---
+++ 0x06-signals/102-signals_block.c
@@ -0,0 +1,36 @@
+#include "signals.h"
+
+/**
+ * sigset_init - Initialises sigset
+ * @set: pointer to signal set to initialise
+ * @signals: 0-terminated array of signals
+ *
+ * Return: 0 on success, -1 on error
+ */
+int sigset_init(sigset_t *set, int *signals)
+{
+	int i;
+
+	if (sigemptyset(set))
+		goto out;
+	for (i = 0; signals[i]; ++i)
+		if (sigaddset(set, signals[i]))
+out:
+			return (-1);
+	return (0);
+}
+
+/**
+ * signals_block - blocks given set of signals
+ * @signals: 0-terminated array of signals
+ *
+ * Return: 0 on success, -1 on error
+ */
+int signals_block(int *signals)
+{
+	sigset_t set;
+
+	if (sigset_init(&set, signals))
+		return (-1);
+	return (sigprocmask(SIG_BLOCK, &set, NULL));
+}
