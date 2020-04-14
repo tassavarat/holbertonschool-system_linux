cf9bfdf3ecb88376798f7d8720d8a58867d74dcf
tassavarat <tassavarat@gmail.com>
2020-04-13 21:40:30 -0700

    Implementing sigset init

---
+++ 0x06-signals/101-sigset_init.c
@@ -0,0 +1,21 @@
+#include "signals.h"
+
+/**
+ * sigset_init - Initialises sigset
+ * @set: pointer to signal set to initialise
+ * @signals: 0-terminated array of signals
+ *
+ * Return: 0 on success, -1 on error
+ */
+int sigset_init(sigset_t *set, int *signals)
+{
+	int i;
+
+	if (sigemptyset(set))
+		goto out;
+	for (i = 0; signals[i]; ++i)
+		if (sigaddset(set, signals[i]))
+out:
+			return (-1);
+	return (0);
+}
