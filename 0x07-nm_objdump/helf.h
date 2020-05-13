2aeb92329754125c3017ecb99493af4bbdc61b59
tassavarat <tassavarat@gmail.com>
2020-05-12 23:35:32 -0700

    Initial commit

---
+++ 0x07-nm_objdump/helf.h
@@ -0,0 +1,38 @@
+#ifndef HELF_H
+#define HELF_H
+
+#include <elf.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define IS_32 (hdr->Ehdr64->e_ident[EI_CLASS] == ELFCLASS32)
+#define IS_MSB (hdr->Ehdr64->e_ident[EI_DATA] == ELFDATA2MSB)
+#define SET_EHDR(x) (IS_32 ? (void *) &(hdr->Ehdr32->x) : (void *) &(hdr->Ehdr64->x))
+#define SET_SHDR(x, i) (IS_32 ? (void *) &(hdr->Shdr32[i].x) : (void *) &(hdr->Shdr64[i].x))
+#define SET_YHDR(x, i) (IS_32 ? (void *) &(hdr->Sym32[i].x) : (void *) &(hdr->Sym64[i].x))
+#define GET_EHDR(x) (IS_32 ? hdr->Ehdr32->x : hdr->Ehdr64->x)
+#define GET_SHDR(x, i) (IS_32 ? hdr->Shdr32[i].x : hdr->Shdr64[i].x)
+#define GET_YHDR(x, i) (IS_32 ? hdr->Sym32[i].x : hdr->Sym64[i].x)
+
+/**
+ *  struct hdrs - contains 32 and 64 bit ELF header information
+ * @Ehdr32: 32-bit ELF header
+ * @Ehdr64: 64-bit ELF header
+ */
+typedef struct hdrs
+{
+	char *addr;
+	Elf32_Ehdr *Ehdr32;
+	Elf64_Ehdr *Ehdr64;
+	int is_32;
+	int is_msb;
+	Elf32_Shdr *Shdr32;
+	Elf64_Shdr *Shdr64;
+	Elf32_Sym *Sym32;
+	Elf64_Sym *Sym64;
+} hdrs;
+
+#endif /* HELF_H */
---
+++ 0x07-nm_objdump/hnm.c
@@ -0,0 +1,177 @@
+#include "helf.h"
+
+/**
+ * convert_msb - convert to big endian by reversing given array
+ * @ptr: pointer to first octet of word
+ * @size: size of type ptr points to
+ */
+void convert_msb(char *ptr, size_t size)
+{
+	size_t start, end;
+
+	for (start = 0, end = size - 1; start < end; ++start, --end)
+	{
+		ptr[start] ^= ptr[end];
+		ptr[end] ^= ptr[start];
+		ptr[start] ^= ptr[end];
+	}
+}
+
+void read_sym(hdrs *hdr)
+{
+	int i;
+	size_t j, sym_size;
+	char *str_table;
+
+	for (i = 0; i < GET_EHDR(e_shnum); ++i)
+		if (GET_SHDR(sh_type, i) == SHT_SYMTAB)
+			break;
+	str_table = &hdr->addr[GET_SHDR(sh_offset, i + 1)];
+	/* printf("%s\n", str_table + 1); */
+	hdr->Sym32 = (Elf32_Sym *) &hdr->addr[GET_SHDR(sh_offset, i)];
+	hdr->Sym64 = (Elf64_Sym *) &hdr->addr[GET_SHDR(sh_offset, i)];
+	sym_size = GET_SHDR(sh_size, i) / GET_SHDR(sh_entsize, i);
+	/* printf("%lu\n", sym_size); */
+	for (j = 1; j < sym_size; ++j)
+	{
+		if (IS_MSB)
+		{
+			convert_msb((char *) SET_YHDR(st_value, j),
+					sizeof(GET_YHDR(st_value, j)));
+			convert_msb((char *) SET_YHDR(st_info, j),
+					sizeof(GET_YHDR(st_info, j)));
+			convert_msb((char *) SET_YHDR(st_name, j),
+					sizeof(GET_YHDR(st_name, j)));
+		}
+		if ((GET_YHDR(st_info, j) & 0xf) == STT_SECTION || (GET_YHDR(st_info, j) & 0xf) == STT_FILE)
+			continue;
+		printf("%0*lx %s\n", IS_32 ? 8 : 16, GET_YHDR(st_value, j),
+				&str_table[GET_YHDR(st_name, j)]);
+	}
+}
+
+/**
+ * checkbitend - checks system bit and endian
+ * @hdr: struct containing elf header information
+ * @arg_str: string containing name of program
+ */
+void checkbitend(hdrs *hdr)
+{
+	int i;
+
+	hdr->Ehdr32 = (Elf32_Ehdr *) hdr->addr;
+	hdr->Ehdr64 = (Elf64_Ehdr *) hdr->addr;
+	if (IS_MSB)
+	{
+		printf("is_32 %i\n", IS_32);
+		convert_msb((char *) SET_EHDR(e_shoff),
+				sizeof(GET_EHDR(e_shoff)));
+		convert_msb((char *) SET_EHDR(e_shentsize),
+				sizeof(GET_EHDR(e_shentsize)));
+		convert_msb((char *) SET_EHDR(e_shnum),
+				sizeof(GET_EHDR(e_shnum)));
+	}
+	hdr->Shdr32 = (Elf32_Shdr *) &hdr->addr[GET_EHDR(e_shoff)];
+	hdr->Shdr64 = (Elf64_Shdr *) &hdr->addr[GET_EHDR(e_shoff)];
+	if (IS_MSB)
+		for (i = 0; i < GET_EHDR(e_shnum); ++i)
+		{
+			printf("i is %i\n", i);
+			convert_msb((char *) SET_SHDR(sh_type, i),
+					sizeof(GET_SHDR(sh_type, i)));
+			convert_msb((char *) SET_SHDR(sh_offset, i),
+					sizeof(GET_SHDR(sh_offset, i)));
+			convert_msb((char *) SET_SHDR(sh_size, i),
+					sizeof(GET_SHDR(sh_size, i)));
+			convert_msb((char *) SET_SHDR(sh_entsize, i),
+					sizeof(GET_SHDR(sh_entsize, i)));
+		}
+}
+
+/**
+ * getelf - get ELF information
+ * @fp: pointer to file stream
+ * @prog_name: name of program
+ * @arg_str: pointer to name of file to get ELF information from
+ * @exit_stat: current exit status to update accordingly
+ */
+void getelf(FILE *fp, char *prog_name, char *arg_str, int *exit_stat)
+{
+	hdrs hdr;
+	struct stat sb;
+
+	memset(&hdr, 0, sizeof(hdr));
+	if (stat(arg_str, &sb))
+	{
+		perror(NULL);
+		*exit_stat = 1;
+		goto out;
+	}
+	hdr.addr = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fileno(fp), 0);
+	/* check errors */
+	checkbitend(&hdr);
+	if (memcmp(hdr.Ehdr64->e_ident, ELFMAG, SELFMAG))
+	{
+		fprintf(stderr, "%s: %s: file format not recognized\n",
+				prog_name, arg_str);
+		*exit_stat = 1;
+		goto out;
+	}
+	read_sym(&hdr);
+	/* puts("No error"); */
+out:
+	fclose(fp);
+}
+
+/**
+ * parse_args - check for usage errors
+ * @argc: count of arguments
+ * @argv: pointer to array containing arguments
+ * @i: index for argv
+ *
+ * Return: pointer to file stream, otherwise NULL
+ */
+FILE *parse_args(int argc, char *argv[], int i)
+{
+	FILE *fp;
+	struct stat sb;
+
+	if (argc == 1)
+		argv[1] = "a.out";
+	stat(argv[i], &sb);
+	if (S_ISDIR(sb.st_mode))
+	{
+		fprintf(stderr, "%s: Warning: '%s' is a directory\n",
+				argv[0], argv[i]);
+		return (NULL);
+	}
+	fp = fopen(argv[i], "r+b");
+	if (!fp)
+	{
+		fprintf(stderr, "%s: '%s': No such file\n", argv[0], argv[i]);
+		return (NULL);
+	}
+	return (fp);
+}
+
+/**
+ * main - entry point
+ * @argc: count of arguments
+ * @argv: pointer to an array containing arguments
+ *
+ * Return: 0 on success, otherwise 1
+ */
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	int exit_stat = 0, i = 1;
+
+	do {
+		fp = parse_args(argc, argv, i);
+		if (!fp)
+			exit_stat = 1;
+		else
+			getelf(fp, argv[0], argv[i], &exit_stat);
+	} while (++i < argc);
+	return (exit_stat);
+}
