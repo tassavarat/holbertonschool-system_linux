5395a84f8712fd910bbd539e8f4b6f24e5efc3d6
tassavarat <tassavarat@gmail.com>
2020-05-13 01:43:59 -0700

    Corrected size for entries

--- 0x07-nm_objdump/hnm.c
+++ 0x07-nm_objdump/hnm.c
@@ -29,65 +29,100 @@ void sym(hdrs *hdr)
 	hdr->Sym32 = (Elf32_Sym *) &hdr->addr[GET_SHDR(sh_offset, i)];
 	hdr->Sym64 = (Elf64_Sym *) &hdr->addr[GET_SHDR(sh_offset, i)];
 	str_table = &hdr->addr[GET_SHDR(sh_offset, i + 1)];
+	/* printf("sizeof st_value 32 %lu\n", sizeof(hdr->Sym32->st_value)); */
+	/* printf("sizeof st_value 64 %lu\n", sizeof(hdr->Sym64->st_value)); */
+	/* printf("sizeof macro %lu\n", SYM_SIZE(st_value, i)); */
+	/* printf("sizeof st_info 32 %lu\n", sizeof(hdr->Sym32->st_info)); */
+	/* printf("sizeof st_info 64 %lu\n", sizeof(hdr->Sym64->st_info)); */
+	/* printf("sizeof macro %lu\n", SYM_SIZE(st_info, i)); */
+	/* printf("sizeof st_name 32 %lu\n", sizeof(hdr->Sym32->st_name)); */
+	/* printf("sizeof st_name 64 %lu\n", sizeof(hdr->Sym64->st_name)); */
+	/* printf("sizeof macro %lu\n", SYM_SIZE(st_name, i)); */
+	/* printf("sh_size\t%lu\n", GET_SHDR(sh_size, i)); */
+	/* printf("sh_entsize\t%lu\n", GET_SHDR(sh_entsize, i)); */
 	sym_size = GET_SHDR(sh_size, i) / GET_SHDR(sh_entsize, i);
 	for (j = 1; j < sym_size; ++j)
 	{
-		if ((GET_YHDR(st_info, j) & 0xf) == STT_SECTION ||
-				(GET_YHDR(st_info, j) & 0xf) == STT_FILE)
+		if ((GET_SYM(st_info, j) & 0xf) == STT_SECTION ||
+				(GET_SYM(st_info, j) & 0xf) == STT_FILE)
 			continue;
 		if (IS_MSB)
 		{
 			conv_msb((char *) SET_YHDR(st_value, j),
-					sizeof(GET_YHDR(st_value, j)));
+					SYM_SIZE(st_value, i));
 			conv_msb((char *) SET_YHDR(st_info, j),
-					sizeof(GET_YHDR(st_info, j)));
+					SYM_SIZE(st_info, i));
 			conv_msb((char *) SET_YHDR(st_name, j),
-					sizeof(GET_YHDR(st_name, j)));
+					SYM_SIZE(st_name, i));
 		}
-		printf("%0*lx %s\n", IS_32 ? 8 : 16, GET_YHDR(st_value, j),
-				&str_table[GET_YHDR(st_name, j)]);
+		printf("%0*lx %s\n", IS_32 ? 8 : 16, GET_SYM(st_value, j),
+				&str_table[GET_SYM(st_name, j)]);
 	}
 }
 
+/**
+ * init_shdr - initialises ElfN_Shdr
+ * @hdr: struct containing elf header information
+ *
+ * Converts entries to msb if needed
+ */
 void init_shdr(hdrs *hdr)
 {
 	int i;
 
 	hdr->Shdr32 = (Elf32_Shdr *) &hdr->addr[GET_EHDR(e_shoff)];
 	hdr->Shdr64 = (Elf64_Shdr *) &hdr->addr[GET_EHDR(e_shoff)];
+	/* printf("sizeof sh_type 32 %lu\n", sizeof(hdr->Shdr32->sh_type)); */
+	/* printf("sizeof sh_type 64 %lu\n", sizeof(hdr->Shdr64->sh_type)); */
+	/* printf("sizeof macro %lu\n", SHDR_SIZE(sh_type, i)); */
+	/* printf("sizeof sh_offset 32 %lu\n", sizeof(hdr->Shdr32->sh_offset)); */
+	/* printf("sizeof sh_offset 64 %lu\n", sizeof(hdr->Shdr64->sh_offset)); */
+	/* printf("sizeof macro %lu\n", SHDR_SIZE(sh_offset, i)); */
+	/* printf("sizeof sh_size 32 %lu\n", sizeof(hdr->Shdr32->sh_size)); */
+	/* printf("sizeof sh_size 64 %lu\n", sizeof(hdr->Shdr64->sh_size)); */
+	/* printf("sizeof macro %lu\n", SHDR_SIZE(sh_size, i)); */
+	/* printf("sizeof sh_entsize 32 %lu\n", sizeof(hdr->Shdr32->sh_entsize)); */
+	/* printf("sizeof sh_entsize 64 %lu\n", sizeof(hdr->Shdr64->sh_entsize)); */
+	/* printf("sizeof macro %lu\n", SHDR_SIZE(sh_entsize, i)); */
 	if (IS_MSB)
 		for (i = 0; i < GET_EHDR(e_shnum); ++i)
 		{
-			printf("i is %i\n", i);
 			conv_msb((char *) SET_SHDR(sh_type, i),
-					sizeof(GET_SHDR(sh_type, i)));
+					SHDR_SIZE(sh_type, i));
 			conv_msb((char *) SET_SHDR(sh_offset, i),
-					sizeof(GET_SHDR(sh_offset, i)));
+					SHDR_SIZE(sh_offset, i));
 			conv_msb((char *) SET_SHDR(sh_size, i),
-					sizeof(GET_SHDR(sh_size, i)));
+					SHDR_SIZE(sh_size, i));
 			conv_msb((char *) SET_SHDR(sh_entsize, i),
-					sizeof(GET_SHDR(sh_entsize, i)));
+					SHDR_SIZE(sh_entsize, i));
 		}
 }
 
 /**
- * checkbitend - checks system bit and endian
+ * init_ehdr - initialises ElfN_Ehdr
  * @hdr: struct containing elf header information
- * @arg_str: string containing name of program
+ *
+ * Converts entries to msb if needed
  */
 void init_ehdr(hdrs *hdr)
 {
 	hdr->Ehdr32 = (Elf32_Ehdr *) hdr->addr;
 	hdr->Ehdr64 = (Elf64_Ehdr *) hdr->addr;
+	/* printf("sizeof e_shoff 32 %lu\n", sizeof(hdr->Ehdr32->e_shoff)); */
+	/* printf("sizeof e_shoff 64 %lu\n", sizeof(hdr->Ehdr64->e_shoff)); */
+	/* printf("sizeof macro %lu\n", EHDR_SIZE(e_shoff)); */
+	/* printf("sizeof e_shentsize 32 %lu\n", sizeof(hdr->Ehdr32->e_shentsize)); */
+	/* printf("sizeof e_shentsize 64 %lu\n", sizeof(hdr->Ehdr64->e_shentsize)); */
+	/* printf("sizeof macro %lu\n", EHDR_SIZE(e_shentsize)); */
+	/* printf("sizeof e_shnum 32 %lu\n", sizeof(hdr->Ehdr32->e_shnum)); */
+	/* printf("sizeof e_shnum 64 %lu\n", sizeof(hdr->Ehdr64->e_shnum)); */
+	/* printf("sizeof macro %lu\n", EHDR_SIZE(e_shnum)); */
 	if (IS_MSB)
 	{
-		printf("is_32 %i\n", IS_32);
-		conv_msb((char *) SET_EHDR(e_shoff),
-				sizeof(GET_EHDR(e_shoff)));
+		conv_msb((char *) SET_EHDR(e_shoff), EHDR_SIZE(e_shoff));
 		conv_msb((char *) SET_EHDR(e_shentsize),
-				sizeof(GET_EHDR(e_shentsize)));
-		conv_msb((char *) SET_EHDR(e_shnum),
-				sizeof(GET_EHDR(e_shnum)));
+				EHDR_SIZE(e_shentsize));
+		conv_msb((char *) SET_EHDR(e_shnum), EHDR_SIZE(e_shnum));
 	}
 }
 
