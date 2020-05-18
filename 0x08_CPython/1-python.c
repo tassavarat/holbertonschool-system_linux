b31e2395496d399bf8cfd950178ad452b818d5e5
tassavarat <tassavarat@gmail.com>
2020-05-17 21:50:04 -0700

    Updating prototype

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -2,16 +2,16 @@
 
 /**
  * print_python_list - print basic info about Python lists
- * @list: Python object
+ * @p: Python object
  */
-void print_python_list(PyObject *list)
+void print_python_list(PyObject *p)
 {
 	Py_ssize_t i;
 
 	printf("[*] Python list info\n[*] Size of the Python List = %li\n"
 			"[*] Allocated = %li\n",
-			Py_SIZE(list), ((PyListObject *) list)->allocated);
-	for (i = 0; i < Py_SIZE(list); ++i)
+			Py_SIZE(p), ((PyListObject *) p)->allocated);
+	for (i = 0; i < Py_SIZE(p); ++i)
 		printf("Element %li: %s\n",
-				i, Py_TYPE((PyList_GetItem(list, i)))->tp_name);
+				i, Py_TYPE((PyList_GetItem(p, i)))->tp_name);
 }
