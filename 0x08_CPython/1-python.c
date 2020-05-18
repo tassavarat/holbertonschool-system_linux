c3d4f5aa9c625cd91147a5746f6690d0f89805cc
tassavarat <tassavarat@gmail.com>
2020-05-17 20:19:38 -0700

    Initial implementation of Python lists

---
+++ 0x08_CPython/1-python.c
@@ -0,0 +1,14 @@
+#include <python3.8/Python.h>
+#include <stdio.h>
+
+void print_python_list(PyObject *list)
+{
+	Py_ssize_t i;
+
+	printf("[*] Python list info\n[*] Size of the Python List = %li\n"
+			"[*] Allocated = %li\n",
+			Py_SIZE(list), ((PyListObject *) list)->allocated);
+	for (i = 0; i < Py_SIZE(list); ++i)
+		printf("Element %li: %s\n",
+				i, Py_TYPE((PyList_GetItem(list, i)))->tp_name);
+}
