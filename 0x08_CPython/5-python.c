8aae958002e1805f083bff8c6e50ab10dfaf4323
tassavarat <tassavarat@gmail.com>
2020-05-22 22:44:22 -0700

    Implementing print python int

---
+++ 0x08_CPython/5-python.c
@@ -0,0 +1,29 @@
+#include <Python.h>
+
+/**
+ * print_python_int - print basic information about Python int objects
+ * @p: Python object
+ */
+void print_python_int(PyObject *p)
+{
+	unsigned long n;
+	ssize_t size, i;
+	uint32_t *digit;
+
+	if (!PyLong_Check(p))
+	{
+		puts("Invalid Int Object");
+		return;
+	}
+	size = ((PyVarObject *) p)->ob_size;
+	digit = ((PyLongObject *) p)->ob_digit;
+	n = 0;
+	for (i = 0; i < (size < 0 ? size * -1 : size); ++i)
+		n += digit[i] * (1L << (PyLong_SHIFT * i));
+	if (size < 0)
+		putchar('-');
+	if (size && !n)
+		puts("C unsigned long int overflow");
+	else
+		printf("%lu\n", n);
+}
