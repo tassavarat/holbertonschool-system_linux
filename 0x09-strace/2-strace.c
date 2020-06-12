a09553fdd3c03e9588ec58e501da774f5dfdd08e
tassavarat <tassavarat@gmail.com>
2020-06-11 19:44:32 -0700

    More explicit return checking

--- 0x09-strace/2-strace.c
+++ 0x09-strace/2-strace.c
@@ -2,6 +2,29 @@
 #include "syscalls.h"
 
 /**
+ * ret - prints return value of system call
+ * @pid: id of process to print
+ * @sysnum: system call value
+ *
+ * Return: 0 on success, 1 on error
+ */
+int ret(pid_t pid, long sysnum)
+{
+	long retval;
+
+	if (syscalls_64_g[sysnum].ret == VOID)
+		puts(" = ?");
+	else
+	{
+		retval = ptrace(PTRACE_PEEKUSER, pid, sizeof(long) * RAX);
+		if (retval == -1)
+			return (1);
+		printf(" = %#lx\n", retval);
+	}
+	return (0);
+}
+
+/**
  * trace_sysname_ret - print out system call name and return value of process
  * @pid: id of process to trace
  *
@@ -13,22 +36,25 @@ int trace_sysname_ret(pid_t pid)
 	long sysnum;
 
 	setbuf(stdout, NULL);
-	if (waitpid(pid, &wstatus, 0) < 0)
+	if (waitpid(pid, &wstatus, 0) == -1)
 		return (1);
-	ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD);
+	if (ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD) == -1)
+		return (1);
 	while (1)
 	{
 		if (!step_syscall(pid))
 			break;
 		sysnum = ptrace(PTRACE_PEEKUSER, pid, sizeof(long) * ORIG_RAX);
+		if (sysnum == -1)
+			return (1);
 		printf("%s", syscalls_64_g[sysnum].name);
 		if (!step_syscall(pid))
 			break;
-		syscalls_64_g[sysnum].ret == VOID ? puts(" = ?") :
-			printf(" = %#lx\n", ptrace(PTRACE_PEEKUSER, pid, sizeof(long) * RAX));
+		if (ret(pid, sysnum))
+			return (1);
 	}
-	syscalls_64_g[sysnum].ret == VOID ? puts(" = ?") :
-		printf(" = %#lx\n", ptrace(PTRACE_PEEKUSER, pid, sizeof(long) * RAX));
+	if (ret(pid, sysnum))
+		return (1);
 	return (0);
 }
 
@@ -46,10 +72,10 @@ int main(int argc, char *argv[])
 	if (parse_args(argc, argv))
 		return (1);
 	pid = fork();
-	if (pid < 0)
+	if (pid == -1)
 		return (1);
 	if (!pid)
-		return (attach(argv + 1));
+		return (attach(argv + 1) == -1);
 	else
 		return (trace_sysname_ret(pid));
 }
