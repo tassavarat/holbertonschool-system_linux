e5d4f9d1e7667e595ac82da691b09369eba7f18e
tassavarat <tassavarat@gmail.com>
2020-08-20 05:20:08 -0700

    Handling get retrieve all

--- 0x0C-sockets/response.c
+++ 0x0C-sockets/response.c
@@ -7,15 +7,10 @@
  */
 void get_resp(int client_fd, todo_info_t *td_info)
 {
-	char str1[BUFSIZ], str2[BUFSIZ];
-	/* char str1[BUFSIZ]; */
+	char str1[BUFSIZ * 2], str2[BUFSIZ] = {'\0'};
 	todo_list_t *cur;
+	size_t len = GET_CONSTLEN;
 
-	for (cur = td_info->head; cur != NULL; cur = cur->next)
-	{
-		printf("ID: %lu\n", cur->id);
-		printf("TARGET: %lu\n", cur->len);
-	}
 	if (td_info->head == NULL)
 	{
 		sprintf(str1, "%s%s%i\r\n%s[]", RESP_GETOK, "Content-Length: ",
@@ -23,31 +18,30 @@ void get_resp(int client_fd, todo_info_t *td_info)
 	}
 	else
 	{
-		sprintf(str1, "%s%s%lu\r\n%s[%s%lu%s%s%s%s\"}]",
-				RESP_GETOK, "Content-Length: ",
-				td_info->head->len + GET_CONSTLEN, CONTYPE,
-				"{\"id\":", td_info->head->id, ",\"title\":\"",
-				td_info->head->title, "\",\"description\":\"",
-				td_info->head->desc);
-		for (cur = td_info->head->next; cur != NULL; cur = cur->next)
+		for (cur = td_info->head; cur != NULL; cur = cur->next)
 		{
-			/* sprintf(str2, "%lu", td_info->head->id); */
-			/* len += strlen(str2) + strlen(td_info->head->title) + */
-			/* 	strlen(td_info->head->desc); */
-			sprintf(str2, ",%s%s%lu\r\n%s[%s%lu%s%s%s%s\"}]",
-					RESP_GETOK, "Content-Length: ",
-					cur->len + GET_CONSTLEN + 1, CONTYPE,
-					"{\"id\":", cur->id, ",\"title\":\"",
-					cur->title, "\",\"description\":\"",
-					cur->desc);
-			strcat(str1, str2);
+			len += cur->len;
+			if (cur != td_info->head)
+			{
+				++len;
+				sprintf(str1, ",%s%lu%s%s%s%s\"}", "{\"id\":",
+						cur->id, ",\"title\":\"",
+						cur->title,
+						"\",\"description\":\"",
+						cur->desc);
+				strcat(str2, str1);
+			}
 		}
+		sprintf(str1, "%s%s%lu\r\n%s[%s%lu%s%s%s%s\"}]%s",
+				RESP_GETOK, "Content-Length: ",
+				len, CONTYPE, "{\"id\":", td_info->head->id,
+				",\"title\":\"", td_info->head->title,
+				"\",\"description\":\"", td_info->head->desc,
+				str2);
 	}
 	printf("GET /todos -> 200 OK\n");
 	send(client_fd, str1, strlen(str1), 0);
 }
-/* send(client_fd, RESP_OK, RESP_OK_LEN, 0); */
-/* [{"id":0,"title":"Dishes","description":"Not_really_urgent"},{"id":1,"title":"Laundry","description":"To_be_done_by_yesterday"}] */
 
 /**
  * post_resp - formats str for POST response
